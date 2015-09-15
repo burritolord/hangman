4654cb3d51bd16e7e67c09c0029912958c9fb0b0
Nick James <nick.james@workday.com>
2015-09-15 13:14:55 -0700

    basic game working

--- Hangman.js
+++ Hangman.js
@@ -1,27 +1,27 @@
 var has_won = false;
+var has_lost = false;
 var hidden_word = '';
 var correct_guesses = [];
 var num_wrong_guess = 0;
 var hang_man = [
-  '      |    O',
-  '      |    |',
-  '      |   /|',
-  '      |   /|\\',
-  '      |    |',
-  '      |   /',
-  '      |   / \\'
+  '       |    O',
+  '       |    |',
+  '       |   /|',
+  '       |   /|\\',
+  '       |    |',
+  '       |   /',
+  '       |   / \\'
 ];
 
 window.onload = function() {
   // Draw dashed lines
   drawHiddenWord();
 
-  // Handle guessing
-  var guess_button = document.getElementById('guess_button');
-  guess_button.addEventListener('click', main);
+  // Draw letter buttons
+  addGuessingButtons();
 
   // Handle entering a new word
-  var new_word = document.getElementById('hidden_word_button');
+  var new_word = document.getElementById('new_word');
   new_word.addEventListener('click', newWord)
 };
 
@@ -29,36 +29,7 @@ window.onload = function() {
 * Main game logic
 */
 function main(){
-  var guessed_value = getGuessedValue();
-  var previously_guessed_list = getPreviouslyGuessed();
 
-  // Check if guessed value is alphanumeric
-  if (isAlphaNumeric(guessed_value) === false) {
-    alert("Must be alphanumeric");
-    return;
-  }
-
-  // Check if guessed value is in hidden word
-  if(letterInWord(guessed_value)){
-    correct_guesses.push(guessed_value);
-
-    // Update the word being guessed
-    drawHiddenWord();
-  }
-  else {
-    // Check if already guessed
-    if(!hasGuessedBefore(guessed_value, previously_guessed_list)) {
-      previously_guessed_list.push(guessed_value);
-
-      // Update previously guessed list
-      setPreviouslyGuessed(previously_guessed_list.sort().toString());
-
-      // Draw the hangman
-      drawHangMan();
-    }
-  }
-  // Clear out the guess box
-  setGuessedValue('');
 }
 
 /**
@@ -71,31 +42,6 @@ function letterInWord(letter){
 }
 
 /**
- * Check if the guessed value has been guessed before
- *
- * @param needle
- * @param haystack
- */
-function hasGuessedBefore(needle, haystack){
-  return haystack.indexOf(needle) >= 0;
-}
-
-/**
- * Retrieve the guessed value
- */
-function getGuessedValue(){
- return document.getElementById('guess_box').value;
-}
-
-/**
- * Set the value of the input box
- * @param value
- */
-function setGuessedValue(value){
-  document.getElementById('guess_box').value = value;
-}
-
-/**
  * Check if the value guessed is alphanumeric
  */
 function isAlphaNumeric(phrase){
@@ -103,37 +49,15 @@ function isAlphaNumeric(phrase){
 }
 
 /**
- * Return the list of previously guessed values
- */
-function getPreviouslyGuessed(){
-  var previously_guessed = document.getElementById('guessed_list');
-  if (previously_guessed.innerHTML !== "") {
-    return previously_guessed.innerHTML.split(",");
-  }
-
-  return [];
-}
-
-/**
- * Set the value of the previously guessed text
- * @param list
- */
-function setPreviouslyGuessed(list){
-  document.getElementById('guessed_list').innerHTML = list;
-}
-
-/**
  * Draw the hangman
  */
 function drawHangMan(){
   var bad_guess_indicies = [2,3,3,3,4,5,5];
-  if (num_wrong_guess < hang_man.length && !has_won) {
+  if (num_wrong_guess < hang_man.length && !has_won && !has_lost) {
     var game_board = document.getElementById("game_board");
     var board_array = game_board.innerHTML.split("\n");
     board_array[bad_guess_indicies[num_wrong_guess]] = hang_man[num_wrong_guess];
     game_board.innerHTML = board_array.join('\n');
-
-    num_wrong_guess++;
   }
 }
 
@@ -141,7 +65,7 @@ function drawHangMan(){
  * Draw the word that is being guessed
  */
 function drawHiddenWord(){
-  var dash_placeholder = document.getElementById("dash_word");
+  var $dash_placeholder = $("#dash_word");
   var word = [];
   for(var i = 0; i < hidden_word.length; i++){
     if((correct_guesses.indexOf(hidden_word.charAt(i))) >= 0){
@@ -152,36 +76,97 @@ function drawHiddenWord(){
     }
   }
 
-  dash_placeholder.innerHTML = "Word: " + word.join(' ');
+  $dash_placeholder.html("Word: " + word.join(' '));
 }
 
 /**
  * Set a new word
  */
 function newWord(){
-  var new_word = document.getElementById('hidden_word');
-  // Check if guessed value is alphanumeric
-  if (isAlphaNumeric(new_word.value) === false) {
-    alert("Must be alphanumeric");
-    return;
+  var new_word = prompt('Enter new word');
+  if(new_word != null){
+    // Check if guessed value is alphanumeric
+    if (isAlphaNumeric(new_word) === false) {
+      alert("Must be alphanumeric");
+      return;
+    }
+
+    // Clear global variables
+    has_won = false;
+    hidden_word = new_word;
+    correct_guesses = [];
+    num_wrong_guess = 0;
+
+    // Update screen
+    drawHiddenWord();
+    clearHangman();
   }
-
-  // Clear global variables
-  has_won = false;
-  hidden_word = new_word.value;
-  correct_guesses = [];
-  num_wrong_guess = 0;
-
-  // Clear text box
-  new_word.value = '';
-
-  // Update screen
-  drawHiddenWord();
 }
 
 /**
- *
+ * Clear the hang man pic
  */
 function clearHangman() {
-  var post = ' ------\n|\n    |\n    |\n    |\n    |\n    |\n    |\n    |-------';
+  var post = ['       ------\n',
+              '       |\n    |',
+              '       |\n',
+              '       |\n',
+              '       |\n',
+              '       |\n',
+              '\n    |\n    |\n    |\n    |\n    |\n    |-------'];
+  var bad_guess_indicies = [2,3,3,3,4,5,5];
+  var $game_board = $("#game_board");
+  var board_array = $game_board.html().split("\n");
+  for(var i; i < bad_guess_indicies.length; i++){
+    board_array[bad_guess_indicies[i]] = hang_man[num_wrong_guess];
+  }
+
+  game_board.innerHTML = board_array.join('\n');
+}
+
+/**
+ * Draw guessing buttons
+ */
+function addGuessingButtons() {
+  var i, tmp, guessed_val, alpha = "abcdefghijklmnopqrstuvwxyz";
+  var $row, $div = $('#letters');
+
+  $div.append("<div id='row1' class='center'></div>");
+  $div.append("<div id='row2' class='center'></div>");
+  $row = $("#row1");
+  for(i = 0, length = alpha.length/2; i < length; i++){
+    tmp = alpha.charAt(i);
+    $row.append("<button value='" + tmp + "'>" + tmp.toUpperCase() + "</button>");
+  }
+
+  $row = $("#row2");
+  for(i = alpha.length/2; i < alpha.length; i++){
+    tmp = alpha.charAt(i);
+    $row.append("<button value='" + tmp + "'>" + tmp.toUpperCase() + "</button>");
+  }
+
+  // Add handler
+  $('#letters button').click( function(){
+    guessed_val = $(this).val();
+    // Check if guessed value is in hidden word
+    if(letterInWord(guessed_val)){
+      correct_guesses.push(guessed_val);
+
+      // Update the word being guessed
+      drawHiddenWord();
+    }
+    else {
+      // Draw the hangman
+      drawHangMan();
+      num_wrong_guess++;
+    }
+    $(this).prop('disabled', true);
+
+    // disable buttons if lost
+    if(num_wrong_guess === hang_man.length) {
+      $('#letters button').prop('disabled', true);
+      has_won = false;
+      has_lost = true;
+    }
+  });
 }
